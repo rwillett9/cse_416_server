9b99157db7b50a60537ec71ca576d82d4e78cb0c
rwillett9 <rjw4321@gmail.com>
2022-03-05 16:25:44 -0500

    initial commit with starter code

---
+++ sparks/src/main/java/com/sparks/sparks/DistrictsApplication.java
@@ -0,0 +1,13 @@
+package com.sparks.sparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictsApplication.class, args);
+	}
+
+}
