066db2e8370d752303ce803868040c1f9ca9361d
rwillett9 <rjw4321@gmail.com>
2022-04-10 15:30:39 -0400

    started over

--- sparks/src/main/java/com/sparks/example/MessageNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.sparks.example;
-
-// initialize custom Exception for when Message with given id does not exist
-public class MessageNotFoundException extends RuntimeException{
-    MessageNotFoundException(Long id) {
-        super("Could not find Message with id=" + id);
-    }
-}
